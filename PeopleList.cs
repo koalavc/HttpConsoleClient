a119f174a8a55e96e8718d239977170f79d665e1
Vincent Chho <vincec362@gmail.com>
2019-10-16 10:11:15 -0700

    init

---
+++ PeopleList.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpConsoleClient
+{
+    public class Result
+    {
+        public string name { get; set; }
+        public string height { get; set; }
+        //public string mass { get; set; }
+        public double mass { get; set; }
+
+        [JsonProperty("hair_color")]
+        public string HairColor { get; set; }
+
+        [JsonProperty("skin_color")]
+        public string SkinColor { get; set; }
+        public string eye_color { get; set; }
+        public string birth_year { get; set; }
+        public string gender { get; set; }
+        public string homeworld { get; set; }
+        //public List<string> films { get; set; }
+        public List<string> films { get; set; }
+        public List<string> species { get; set; }
+
+        [JsonProperty("vehicles")]
+        public List<string> vehiclesJson { get; set; }
+        public List<string> starships { get; set; }
+        public DateTime created { get; set; }
+        public DateTime edited { get; set; }
+        public string url { get; set; }
+
+        [JsonIgnore]
+        public int NumberOfVehicles
+        {
+            get {
+                return vehiclesJson.Count;
+            }
+        }
+    }
+
+    public class PeopleList
+    {
+        [JsonProperty("count")]
+        public int Count { get; set; }
+        public string Next { get; set; }
+        public object Previous { get; set; }
+
+        [JsonProperty("results")]
+        public List<Result> People { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Http;
+
+namespace HttpConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var http = new HttpClient();
+            //http.BaseAddress = new Uri("https://swapi.co");
+
+            //http.GetAsync("https://swapi.co/api/people/");
+            var jsonString = http.GetStringAsync("https://swapi.co/api/people/").Result;
+
+            var jsonObject = JsonConvert.DeserializeObject<PeopleList>(jsonString);
+            var people = jsonObject.People;
+
+            Console.WriteLine(jsonString);
+
+            var filteredPeople = people.Where(person => person.NumberOfVehicles > 0);
+
+            foreach(var person in filteredPeople)
+            {
+                Console.WriteLine("Name: " + person.name);
+                Console.WriteLine("Birth Year: " + person.birth_year);
+                Console.WriteLine("Hair Color: " + person.HairColor);
+                Console.WriteLine("Eye Color: " + person.eye_color);
+                Console.WriteLine($"Gender: {person.gender}");
+                //var mass2 = Convert.ToInt32(person.mass) + 10;
+                Console.WriteLine($"Mass: {person.mass} \n");
+                Console.WriteLine($"Mass: {person.mass + 10} \n");
+                Console.WriteLine($"Number of Vehicles: {person.NumberOfVehicles}");
+                //Console.WriteLine($"Mass2: {mass2} \n");
+                foreach(var jsonUrl in person.vehiclesJson) 
+                {
+                    Console.WriteLine($"Vehicle: " + jsonUrl);
+                    var result = http.GetStringAsync(jsonUrl).Result;
+                    //var response = new HttpResponseMessage { };
+                    var vehicle = JsonConvert.DeserializeObject<Vehicle>(result);
+                    Console.WriteLine($"Model: {vehicle.model}");
+                    Console.WriteLine($"Manufacturer: {vehicle.manufacturer}");
+                    Console.WriteLine($"Name: {vehicle.name}");
+                    Console.WriteLine($"Number of passengers: {vehicle.passengers}");
+                }
+                Console.WriteLine("");
+            }
+            var jsonStringResult = JsonConvert.SerializeObject(filteredPeople);
+            Console.WriteLine("Returned JSON" + jsonStringResult);
+            Console.ReadKey();
+        }
+    }
+}
